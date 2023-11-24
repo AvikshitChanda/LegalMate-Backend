128d8a854510da48a2906040013a092410c28333
Aryan Kashyap <gabruarya@users.noreply.huggingface.co>
2023-11-24 13:53:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,7 @@ st.markdown(
     🤖 **Getting Started:**
     Feel free to ask any legal question related to Indian law, using keywords like "property rights," "labor laws," or "family law." I'm here to assist you!
     
+    Let's get started! How can I assist you today?
     """
 )
 
@@ -125,7 +126,6 @@ with prompt_placeholder:
     cols = st.columns((6, 1))
     cols[0].text_input(
         "Chat",
-        value="Let's get started! How can I assist you today?",
         label_visibility="collapsed",
         key="human_prompt",
     )
