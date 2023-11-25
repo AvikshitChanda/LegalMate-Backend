d1eb3631d4b82080f955fb24b7685eb36990e048
Aryan Kashyap <gabruarya@users.noreply.huggingface.co>
2023-11-25 13:22:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ st.markdown(
     I'm here to assist you with your legal queries within the framework of Indian law. Whether you're navigating through specific legal issues or seeking general advice, I'm here to help.
     
     📚 **How I Can Assist:**
+    
     - Answer questions on various aspects of Indian law.
     - Guide you through legal processes relevant to India.
     - Provide information on your rights and responsibilities as per Indian legal standards.
@@ -103,9 +104,11 @@ st.markdown(
     Rest assured, your privacy is of utmost importance. All interactions are confidential and secure. No personal details are stored unless you choose to create an account.
     
     ⚖️ **Disclaimer:**
+    
     While I can provide general information, it's essential to consult with a qualified Indian attorney for advice tailored to your specific situation.
     
     🤖 **Getting Started:**
+    
     Feel free to ask any legal question related to Indian law, using keywords like "property rights," "labor laws," or "family law." I'm here to assist you!
 
     Let's get started! How can I assist you today?
